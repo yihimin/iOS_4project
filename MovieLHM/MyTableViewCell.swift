b3c389aa2a5d9743ce9d8acd6c7c69a9cc529bec
yihimin <yihimin01@gmail.com>
2025-05-08 15:48:36 +0900

    테이블뷰

---
+++ MovieLHM/MyTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  MyTableViewCell.swift
+//  MovieLHM
+//
+//  Created by Induk-cs-1 on 2025/05/08.
+//
+
+import UIKit
+
+class MyTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var movieName:  UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- MovieLHM/ViewController.swift
+++ MovieLHM/ViewController.swift
@@ -4,22 +4,11 @@
 //
 //  Created by Induk-cs-1 on 2025/05/08.
 //
-
 import UIKit
 
+let name = ["야당1", "야당2", "야당3", "야당4", "야당5"]
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
-    }
-    
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 3
-    }
-    
+
     @IBOutlet weak var table : UITableView!
     
     override func viewDidLoad() {
@@ -28,6 +17,19 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         table.delegate = self
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath) as! MyTableViewCell
+        cell.movieName.text = name[indexPath.row]
+        return cell
+    }
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 6
+    }
 
 }
 
